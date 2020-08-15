a9cd6e52f50bba2b1b5e18b05f6b78feee2b0d3f
Justin Nguyen <nuggetnchill@gmail.com>
2020-08-15 01:58:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import "./App.scss";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/projects' component={ProjectPage} />
+        <Route path='/contact' component={ContactPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./NavBar.style.scss";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar'>
+      <Link to='/'>
+        <img alt='logo' src={logo} />
+      </Link>
+      <ul>
+        <Link className='link' to='/'>
+          HOME
+        </Link>
+        <span> / </span>
+
+        <Link className='link' to='/about'>
+          ABOUT
+        </Link>
+        <span> / </span>
+
+        <Link className='link' to='/projects'>
+          PROJECTS
+        </Link>
+        <span> / </span>
+
+        <Link className='link' to='/contact'>
+          CONTACT
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
