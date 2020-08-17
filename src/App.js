1114c96d82f1c547c017decc78096d25dafdad9b
Justin Nguyen <nuggetnchill@gmail.com>
2020-08-17 17:49:48 -0500

    added netlify toml

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ProjectPage from './pages/ProjectPage';
-import ContactPage from './pages/ContactPage';
-import ErrorPage from './pages/ErrorPage';
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
+import ContactPage from "./pages/ContactPage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path='/about' component={AboutPage} />
           <Route path='/projects' component={ProjectPage} />
           <Route path='/contact' component={ContactPage} />
-          <Route component={ErrorPage} />
+          <Route path='/404' component={ErrorPage} />
         </Switch>
       </div>
     </BrowserRouter>
